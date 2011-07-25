5739a613fbf2ddb61d1c9e875ceff4ece5d612c2
Tray Torrance <devwork@warrentorrance.com>
2011-07-25 15:31:38 -0700

    Replaced inline_templates with string interpolation

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,7 +1,7 @@
 define nginx::vhost( $enabled = true, $docroot = '/var/www', $conf_root = '/etc/nginx', $log_root = '/var/log/nginx', $bind_addr = '127.0.0.1', $bind_port = '80'  ) {
-  $vhost_root = inline_template("<%= conf_root %>/sites-available")
-  $vhost_conf = inline_template("<%= vhost_root %>/<%= name %>")
-  $vhost_enabled = inline_template("<%= conf_root %>/sites-enabled/<%= name %>")
+  $vhost_root = "${conf_root}/sites-available"
+  $vhost_conf = "${vhost_root}/${name}"
+  $vhost_enabled = "${conf_root}/sites-enabled/${name}"
 
   file {
     $vhost_conf:
