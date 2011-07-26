3ddc63e43dd88f59854ca238d87d7b27f30233e0
Tray Torrance <devwork@warrentorrance.com>
2011-07-25 17:32:25 -0700

    Adjusted vhost define to use scoped variable lookups

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,10 +1,9 @@
-define nginx::vhost( $enabled = true, $docroot = '/var/www', $conf_root = '/etc/nginx', $log_root = '/var/log/nginx', $bind_addr = '127.0.0.1', $bind_port = '80'  ) {
+define nginx::vhost( $enabled = true, $docroot = '/var/www', $bind_addr = '127.0.0.1', $bind_port = '80'  ) {
   
   include nginx
 
-  $vhost_root = "${conf_root}/sites-available"
-  $vhost_conf = "${vhost_root}/${name}"
-  $vhost_enabled = "${conf_root}/sites-enabled/${name}"
+  $vhost_conf    = "${nginx::sites_available}/${name}"
+  $vhost_enabled = "${nginx::sites_enabled}/${name}"
 
   file {
     $vhost_conf:
@@ -12,20 +11,21 @@ define nginx::vhost( $enabled = true, $docroot = '/var/www', $conf_root = '/etc/
       mode    => 0644,
       owner   => root,
       group   => root,
-      require => File[$vhost_root],
+      require => File[$nginx::sites_available],
       content => template('nginx/vhost.conf');
   }
 
-  if $enabled == true {
-    file {
-      $vhost_enabled:
-        ensure  => link,
-        mode    => 0644,
-        owner   => root,
-        group   => root,
-        require => File[$vhost_conf],
-        notify  => Service['nginx'],
-        target  => $vhost_conf;
-    }
+  file {
+    $vhost_enabled:
+      ensure  => $enabled ? {
+        false   => absent,
+        default => link,
+      },
+      mode    => 0644,
+      owner   => root,
+      group   => root,
+      require => File[$vhost_conf],
+      notify  => Service['nginx'],
+      target  => $vhost_conf;
   }
 }
