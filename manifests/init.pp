2df78d1b37056d2143f18a014947a71ef4748478
Tray Torrance <devwork@warrentorrance.com>
2011-07-25 17:20:59 -0700

    Collapsed init.pp directories into variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,20 @@
 import '*'
 
 class nginx( $user = 'www-data', $conf_root = '/etc/nginx', $log_root = '/var/log/nginx', $pidfile = '/var/run/nginx.pid', $workers = 1, $connections = 1024, $keepalive = 60 ) {
+  
+  $conf_d          = "${conf_root}/conf.d"
+  $include_dir     = "${conf_root}/includes"
+  $sites_available = "${conf_root}/sites-available"
+  $sites_enabled   = "${conf_root}/sites-enabled"
+  $main_conf       = "${conf_root}/nginx.conf"
 
   $conf_dirs = [ 
     $conf_root,
-    "${conf_root}/conf.d",
-    "${conf_root}/includes",
-    "${conf_root}/sites-available",
-    "${conf_root}/sites-enabled",
+    $conf_d,
+    $include_dir,
+    $sites_available,
+    $sites_enabled,
   ]
-  
-  $main_conf = "${conf_root}/nginx.conf"
   
   package {
     'nginx':
